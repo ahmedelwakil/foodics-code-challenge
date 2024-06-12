f47d0284de0f7938f8114697a7e659afc190d434
Ahmed Tarek <mido_mavrick@hotmail.com>
2024-06-12 16:57:26 +0300

    Adjust Mail Blade

--- resources/views/emails/stock/below_threshold.blade.php
+++ resources/views/emails/stock/below_threshold.blade.php
@@ -1,7 +1,7 @@
 <x-mail::message>
 # Your Stock has gone below Threshold
 
-Your stock for the ingredient {{$ingredientName}} has gone below threshold. Please update the stock.
+Your stock for the ingredient (**{{$ingredientName}}**) has gone below threshold. Please update the stock.
 
 Thanks,<br>
 </x-mail::message>
